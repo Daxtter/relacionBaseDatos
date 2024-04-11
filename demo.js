e95b96e96e2a8ea35f32a0e3ee456d8fc7b283c3
Manuel <lmnunez2003@gmail.com>
2024-04-11 13:27:30 -0700

    Meta 3.3 ahora si terminada

--- demo.js
+++ demo.js
@@ -1,37 +1,6 @@
 const models = require("./models");
 
 async function demostracion(){
-    /* 
-    await models.Activo.create({
-      numSerie: 1,
-      numInventario: 1,
-      descripcion: "Tableta",
-      imagen: null,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
-    //let activo = models.Activo.findAll();
-     models.Tag.create({
-        nombre: "Dispositivo",
-        createdAt: new Date(),
-        updatedAt: new Date()
-    });
-
-    let activosAAgregar = await models.Activo.findOne({
-        where:{
-            numSerie:1
-        }
-    });
-
-    let tag = await models.Tag.findOne({
-        where: {
-            nombre: "Dispositivo"
-        }
-    });
-
-    await tag.addActivos(activosAAgregar);
-    
-    */
    let activoTableta =await  models.Activo.findOne({
     where:{
         descripcion:"Tableta"
@@ -62,3 +31,4 @@ async function demostracion(){
     models.sequelize.close();
 }
 demostracion();
+/////////////////jhbujhbhjb
